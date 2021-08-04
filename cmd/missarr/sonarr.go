c048b1fd88669a231c9357758e37f05f8da5e177
l3uddz <l3uddz@gmail.com>
2021-08-04 08:25:06 +0100

    sonarr: sort missing into series

--- cmd/missarr/sonarr.go
+++ cmd/missarr/sonarr.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/l3uddz/missarr/sonarr"
 	"github.com/rs/zerolog/log"
+	"time"
 )
 
 type SonarrCmd struct {
@@ -29,5 +30,18 @@ func (r *SonarrCmd) Run(c *config) error {
 	}
 
 	log.Info().Int("size", len(epps)).Msg("Retrieved missing")
+
+	// sort missing into seasons
+	series := make(map[int]time.Time)
+	for _, e := range epps {
+		// seen before?
+		if _, ok := series[e.SeriesId]; ok {
+			continue
+		}
+		// add to map
+		series[e.SeriesId] = e.AirDateUtc
+	}
+	log.Info().Int("series", len(series)).Msg("Sorted missing into unique series")
+
 	return nil
 }
