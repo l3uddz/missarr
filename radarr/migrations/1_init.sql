383efb0d6dcc8d1a00be13206063d824db9ae0b1
l3uddz <l3uddz@gmail.com>
2022-04-13 11:25:06 +0100

    fix(migrations): ensure migrations are ran in version order

--- radarr/migrations/1_init.sql
+++ radarr/migrations/1_init.sql
@@ -2,6 +2,5 @@ CREATE TABLE IF NOT EXISTS movies (
     "movie" INTEGER NOT NULL,
     "release_date" DATETIME NOT NULL,
     "search_date" DATETIME NULL,
-    "type" VARCHAR(10),
     PRIMARY KEY(movie)
 )
